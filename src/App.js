c6fd576c32e87494d5a99f61f86a917958ee7470
priya1chaurasiya2 <priya1chaurasiya2@gmail.com>
2018-07-15 14:25:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Link from './Link.react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Link/>
+      </div>
+    );
+  }
+}
+
+export default App;
